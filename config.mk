76bb1e56107e8911c4847040b72f96bbb4515b05
Ben O'Neill <ben@benoneill.xyz>
2019-11-20 18:08:58 -0500

    C89 -> C99

--- config.mk
+++ config.mk
@@ -10,6 +10,6 @@ CC=$(ARCH_PREFIX)-gcc
 LD=$(CC)
 ASFLAGS=-Wall
 CPPFLAGS = -DARCH="\"$(ARCH)\"" -DVERSION="\"$(VERSION)\"" -DDEBUG
-CFLAGS= $(CPPFLAGS) -Wall -O3 -ffreestanding -fno-pie -std=c89 -pedantic -I$(SRC_DIR)/drivers
+CFLAGS= $(CPPFLAGS) -Wall -O3 -ffreestanding -fno-pie -std=c99 -pedantic -I$(SRC_DIR)/drivers
 LDFLAGS=-ffreestanding -fno-stack-protector -fPIC -nostdlib -T$(ARCH_SRC_DIR)/linker.ld
 TARGET=$(ARCH)-swerve-$(VERSION)
