f53eefb1a580134e176e065f29522a960d8b831f
Ben O'Neill <ben@benoneill.xyz>
2019-11-21 00:52:12 -0500

    Remove RS-232 shell stuff

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -19,65 +19,25 @@
 
 #include "kmain.h"
 
-#include <stdbool.h>
-
 #include "drivers/tty.h"
 #include "drivers/serial/rs232.h"
 #include "mm/wmalloc.h"
 
-const char *version_info = "swerve-" VERSION "-" ARCH;
+const char *version_info = "swerve v" VERSION "-" ARCH;
 
-/* FIXME put in libc */
-bool streq(const char *s1, const char *s2)
+/* FIXME put strcmp in libc and use that instead */
+int streq(const char *s1, const char *s2)
 {
 	int i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0') {
 		if (s1[i] != s2[i])
-			return false;
+			return 0;
 		i++;
 	} 
 	if (s1[i] != '\0' || s2[i] != '\0')
-		return false;
+		return 0;
 
-	return true;
-}
-
-void rs232_shell(void)
-{
-	char *input;
-	int i;
-	char c;
-	bool running, reading;
-
-	running = true;
-	while (running) {
-		input = wmalloc(256);
-		i = 0;
-		reading = true;
-		rs232_print("$ ");
-		while (reading) {
-			input[i] = rs232_getchar();
-			rs232_putchar(input[i]);
-			if (input[i] == '\r') {
-				input[i] = '\0';
-				rs232_putchar('\n');
-				reading = false;
-			}
-			i++;
-		}
-
-		if (streq(input, "help")) {
-			rs232_println("commands: help, exit, version");
-		} else if (streq(input, "exit")) {
-			running = false;
-		} else if (streq(input, "version")) {
-			rs232_println(version_info);
-		} else {
-			rs232_println("unrecognized command. type help for command list");
-		}
-	}
-
-	rs232_println("Bye!");
+	return 1;
 }
 
 void kmain(void)
@@ -95,7 +55,6 @@ void kmain(void)
 #endif
 
 	/* print welcome */
-	terminal_print("Version: ");
 	terminal_println(version_info);
 	terminal_disable_cursor();
 #ifdef DEBUG
@@ -107,10 +66,4 @@ void kmain(void)
 #ifdef DEBUG
 	rs232_println("* Initialized watermark memory allocator");
 #endif
-
-	/* start RS-232 shell */
-#ifdef DEBUG
-	rs232_println(". Starting RS-232 shell");
-#endif
-	rs232_shell();
 }
