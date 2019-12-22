4f971e64482fca7c6f7b54b7c6728566efcab0a8
Ben O'Neill <ben@benoneill.xyz>
2019-12-22 16:11:04 -0500

    Change copyright scheme

---
+++ AUTHORS.md
@@ -0,0 +1,3 @@
+# Author List for `swerve`
+
+1. Ben O'Neill: <ben@benoneill.xyz>
--- kernel/arch/i386/drivers/terminal.c
+++ kernel/arch/i386/drivers/terminal.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the swerve kernel.
  *
- * Copyright (C) 2019 Benjamin O'Neill <ben@benoneill.xyz>
+ * Copyright (C) 2019 the swerve development team (see AUTHORS.md).
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
