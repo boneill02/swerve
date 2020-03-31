bdb514e0bafb15722252336e0b7d3fc8e7ea8320
Ben O'Neill <ben@benoneill.xyz>
2020-03-31 14:46:39 -0400

    Optimize for size, and remove DEBUG

--- config.mk
+++ config.mk
@@ -3,7 +3,6 @@ VERSION = 0.01a-dev
 # user configurable options
 ARCH = i386
 ARCH_PREFIX = $(ARCH)-swerve
-DEBUG = 1
 LIBC_INCLUDE = "-I/usr/local/include/swerve"
 LIBC_LIB_DIR = "-L/usr/local/lib/swerve"
 
@@ -15,8 +14,8 @@ LD = $(CC)
 AR = $(ARCH_PREFIX)-ar
 RANLIB = $(ARCH_PREFIX)-ranlib
 ASFLAGS = -W
-CPPFLAGS = -DARCH="\"$(ARCH)\"" -DVERSION="\"$(VERSION)\"" -DDEBUG -D__is_libk $(DRIVERS)
-CFLAGS = $(CPPFLAGS) -Wall -O3 -ffreestanding -fno-pie -std=c99 -pedantic -nostdinc \
+CPPFLAGS = -DARCH="\"$(ARCH)\"" -DVERSION="\"$(VERSION)\"" $(DRIVERS)
+CFLAGS = $(CPPFLAGS) -Wall -Os -ffreestanding -fno-pie -std=c99 -pedantic -nostdinc \
 		-I$(SRC_DIR)/drivers $(LIBC_INCLUDE)
 LDFLAGS = -nostdlib -static -ffreestanding -fno-stack-protector \
 		 -fPIC -T$(ARCH_SRC_DIR)/linker.ld $(LIBC_LIB_DIR)
